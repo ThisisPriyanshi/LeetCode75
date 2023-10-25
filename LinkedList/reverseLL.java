531686d55d81e7cede5ec4d993a5728b831a0a8e
Priyanshi Singh <136875033+ThisisPriyanshi@users.noreply.github.com>
2023-10-25 13:54:52 +0530

    Add files via upload

---
+++ LinkedList/reverseLL.java
@@ -0,0 +1,48 @@
+public class reverseLL {
+
+    //Recursion
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null)
+        {
+            return head;
+        }
+
+        return reverse(null, head);
+    }
+
+    private ListNode reverse(ListNode prev, ListNode curr)
+    {
+        if(curr == null)
+        {
+            return prev;
+        }
+
+        ListNode nextNode = curr.next;
+        curr.next = prev;
+        return reverse(curr, nextNode);
+    }
+
+    //Iterative
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null)
+        {
+            return head;
+        }
+
+        return reverse(null, head);
+    }
+
+    private ListNode reverse(ListNode prev, ListNode curr)
+    {
+        if(curr == null)
+        {
+            return prev;
+        }
+
+        ListNode nextNode = curr.next;
+        curr.next = prev;
+        return reverse(curr, nextNode);
+    }
+}
